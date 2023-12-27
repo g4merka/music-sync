f9805c4a87fa1ed0630dabdffc48be71bf3d3e3b
g4merka <diakonov54@ya.ru>
2023-12-27 11:24:11 +0500

    Поработал с POST-зарпосами и отправкой форм

--- express/app.js
+++ express/app.js
@@ -1,14 +1,16 @@
-const express = require("express"); //подключение Express.JS
-const app = express(); //создаем объект приложения
-app.get("/", function(request, response){ //определяем обработчик для маршрута "/"
-      // ↑ маршрут, ↑ обработчик
-    response.send("<h2>Привет Express.JS!</h2>"); //отправляем ответ
-});
-app.get("/about", function(req, res){
-    res.send("<h1>О сайте</h1>");
-});
-app.get("/contact", function(req, res){
-    res.send("Контакты");
+const express = require("express") //подключение Express.JS
+const fs = require("fs")
+const app = express() //создаем объект приложения
+
+const urlencodedParser = express.urlencoded({extended: false}) // создали парсер данных application/x-www-form-urlencoded для получения отправленных данных
+                                                        //↑ объект - результат парсинга будет представлять набор пар ключ-значение, а каждое значение может быть представлено в виде строки или массива
+app.get("/", function(_, res){
+    res.sendFile(__dirname + "/index.html") // открывает index.html на главной /
 })
-app.listen(3000); //прослушиваем подключения на 3000 порту
+app.post("/", urlencodedParser, function(req, res){ // 1 парам. "/" можно было не писать, т. к. по умолчанию данные из index.html отправляются по тому же адресу, где находится стр. - /
+    if(!req.body) return res.sendStatus(400)
+    console.log(req.body)
+    res.send(`${req.body.userName} - ${req.body.userAge}`)
+})
+app.listen(3000, () => console.log("Сервер запущен...")) //прослушиваем подключения на 3000 порту
 //↑ метод для запуска сервера
