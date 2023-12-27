cc0dd3eb266d64d444369ca6b9919d935251c0fd
g4merka <diakonov54@ya.ru>
2023-12-27 16:27:43 +0500

    Поигрался с параметрами маршрута

--- express/app.js
+++ express/app.js
@@ -4,13 +4,20 @@ const app = express() //создаем объект приложения
 
 const urlencodedParser = express.urlencoded({extended: false}) // создали парсер данных application/x-www-form-urlencoded для получения отправленных данных
                                                         //↑ объект - результат парсинга будет представлять набор пар ключ-значение, а каждое значение может быть представлено в виде строки или массива
-app.get("/", function(_, res){
-    res.sendFile(__dirname + "/index.html") // открывает index.html на главной /
+app.get("/users/:userId", function(req, res){
+    res.send("userId: " + req.params["userId"]) // значение указывается в конце маршрута ../users/<int>
 })
-app.post("/", urlencodedParser, function(req, res){ // 1 парам. "/" можно было не писать, т. к. по умолчанию данные из index.html отправляются по тому же адресу, где находится стр. - /
-    if(!req.body) return res.sendStatus(400)
-    console.log(req.body)
-    res.send(`${req.body.userName} - ${req.body.userAge}`)
+
+app.get("/type/:media.:ext", function(req, res){ // запрос: ../type/<media>.<ext>
+    const media = req.params["media"]
+    const ext = req.params['ext']
+    res.send(`Полное имя файла: ${media}.${ext}`)
 })
+app.get("/type/:media/extension:ext", function(req, res){ // запрос: ../type/<media>/extension/<ext>
+    const media = req.params["media"]
+    const ext = req.params['ext']
+    res.send(`Тип: ${media} Расширение: ${ext}`)
+})
+
 app.listen(3000, () => console.log("Сервер запущен...")) //прослушиваем подключения на 3000 порту
 //↑ метод для запуска сервера
